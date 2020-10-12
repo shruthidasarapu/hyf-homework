6f67c657026df583bb039f23a5c199386c671949
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2020-10-12 14:08:32 +0200

    added name of database

--- databases/week1/hw.sql
+++ databases/week1/hw.sql
@@ -1,51 +1,59 @@
-  SELECT  * FROM test.task
+  USE test;
+  SELECT  * FROM task
 // How many tasks are in the task table
  
-    SELECT COUNT(title) FROM test.task;
+    SELECT COUNT(id) FROM task;
  
 // How many tasks in the task table do not have a valid due date
-   SELECT  * FROM test.task
-   WHERE due_date is null;
+   SELECT  * FROM task
+   WHERE due_date IS NULL;
    
 // All the tasks that are marked as done
-   SELECT  * FROM test.task
+   SELECT  * FROM task
    WHERE status_id = 3;
    
 // All the tasks that are not marked as done
-   SELECT  * FROM test.task
+   SELECT  * FROM task
    WHERE status_id != 3;
    
 // All the tasks, sorted with the most recently created first
-     SELECT  * FROM test.task
-     ORDER BY created DESC
+     SELECT  * FROM task
+     ORDER BY created DESC;
      
 // The single most recently created task
-      SELECT  * FROM test.task
-       ORDER BY created DESC
-       LIMIT 1 ;
+      SELECT  * FROM task
+      ORDER BY created DESC
+      LIMIT 1 ;
        
 //  The title and due date of all tasks where the title or description contains database
       SELECT title, description
-       FROM test.task
+       FROM task
       Where title LIKE '%database%' 
       OR 
-      description LIKE '%database%' 
+      description LIKE '%database%';
      
 // The title and status (as text) of all tasks
-      SELECT  title, name  AS status FROM test.status
-      JOIN test.task
-      ON test.status.id = test.task.status_id;
+      SELECT  title, name  AS status FROM status
+      JOIN task
+      ON status.id = task.status_id;
   
 // Name of each status, along with a count of how many tasks have that status
 
-    SELECT COUNT(test.task.id) AS count, 
+    SELECT COUNT(task.id) AS count, 
          name 
-    FROM   test.status 
-         JOIN test.task 
-           ON test.status.id = test.task.status_id 
-    GROUP  BY test.status.name 
+    FROM   status 
+         JOIN task 
+           ON status.id = task.status_id 
+    GROUP  BY status.name; 
   
 // The names of all statuses, sorted by the status with most tasks first
   
+    
+    SELECT COUNT(task.id) AS count, 
+         name 
+    FROM   status 
+         JOIN task 
+           ON status.id = task.status_id 
+    GROUP  BY status.name 
     ORDER  BY count DESC; 
   
