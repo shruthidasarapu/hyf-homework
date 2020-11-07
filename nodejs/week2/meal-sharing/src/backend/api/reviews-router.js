4d89ec27f57cf5e8881a9ce223f56d426b36d2cd
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2020-11-07 23:56:52 +0100

    Added nodejs/week2 homework

---
+++ nodejs/week2/meal-sharing/src/backend/api/reviews-router.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+
+const reviews = require("./../data/reviews.json");
+
+
+router.get("/", async (request, response) => {
+    response.send(reviews);
+});
+router.get("/:id", async (request, response) => {
+         
+    const randomReviewId = parseInt(request.params.id); 
+    if(!isNaN(randomReviewId)){
+        
+        const reviewWithId = reviews.filter((review) =>  review.id === randomReviewId);
+          
+        response.send(reviewWithId[0]);  
+         
+    } else  { 
+        
+          response.status(400)
+          response.send(`there is no review with this id `);
+    }
+
+
+});
+
+module.exports = router;
