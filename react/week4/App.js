b7b11d244aeb9a80a1f3813bd6edefb11b7e9eb4
shruthidasarapu <66563360+shruthidasarapu@users.noreply.github.com>
2020-12-18 21:45:08 +0100

    Added week4 homework

---
+++ react/week4/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Profile} from './Profile.js'
+
+import {Loader} from './loader.js'
+function App() {
+  return (
+    <div className="App">
+    
+     <Profile></Profile>
+     
+    </div>
+  );
+}
+
+export default App;
